d0f7b660fb54bab11746997483274a9995c51455
hiddevoogt <hiddevoogt@gmail.com>
2024-11-03 10:52:50 +0100

    update

--- Function_Travel_Hours.py
+++ Function_Travel_Hours.py
@@ -1,14 +1,17 @@
+
+# This function defines the graphs that are used for measuring the effect of covid on travel hours over different levels of urbanisation
+
 def plot_travelminutes():
     import pandas as pd
     import plotly.express as px
 
-    # Load the data
+    
     df = pd.read_csv("values_named_clean_mobility_data.csv")
 
-    # Group and aggregate data for the line plot
+    # Group the data for the line plot
     travelmode_animation = df.groupby(['RegionCharacteristics', 'Period']).sum('Time_Travelled_Hours_Per_Year').reset_index()
 
-    # Line plot with improved labels and layout
+    # Code for lineplot
     fig = px.line(
         travelmode_animation, 
         x='Period', 
@@ -22,14 +25,14 @@ def plot_travelminutes():
         title="Travel Time Over Time by Region",
         markers=True
     )
-    
+    # Layout of graph
     fig.update_layout(
         title_font=dict(size=20),
-        title_x=0.5,  # Center the title
+        title_x=0.5,  
         xaxis_title="Period",
         yaxis_title="Average Hours Travelled Per Year",
         legend_title="Regions",
-        template="plotly_white",  # Use a clean template for aesthetics
+        template="plotly_white",  
         font=dict(family="Arial", size=14)
     )
     
@@ -37,8 +40,10 @@ def plot_travelminutes():
 
 
 
-#NORMAL HEAT MAP CODE
+    # Code for heatmap
 
+
+    # Fitting the code for the heatmap
     heatmap_data = travelmode_animation.pivot(index='RegionCharacteristics', columns='Period', values='Time_Travelled_Hours_Per_Year')
 
     fig = px.imshow(heatmap_data, 
